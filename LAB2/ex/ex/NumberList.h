93ccc028060b006dc9249a9606ab4bd7769e0cf9
EmmaGirigan <75792071+EmmaGirigan@users.noreply.github.com>
2022-03-11 10:27:56 +0200

    lab2-ex1

---
+++ LAB2/ex/ex/NumberList.h
@@ -0,0 +1,34 @@
+#pragma once
+#include<iostream>
+
+void NumberList::Init() {
+	count = 0;
+}
+
+bool NumberList::Add(int x) {
+	if (count >= 10) {
+		return false;
+	}
+	numbers[count] = x;
+	count++;
+	return true;
+}
+
+void NumberList::Sort() {
+	for (int i = 0; i <= count; i++) {
+		for (int j = i+1; j < count; j++) {
+			if (numbers[i] > numbers[j]) {
+				int aux = numbers[i];
+				numbers[i] = numbers[j];
+				numbers[j] = aux;
+			}
+		}
+	}
+}
+
+void NumberList::Print() {
+	for (int i = 0; i < count; i++) {
+		std::cout << numbers[i] << " ";
+	}
+	std::cout << "\n";
+}
